a507395280a6592adef36902955ff0848f123db4
Ingrid M Miranda <ingridmoreiramiranda@gmail.com>
2023-06-24 18:57:41 -0300

    Adicionado: - retornando id em listagem;

--- src/main/java/com/cadCompany/desafio/fornecedor/DadosListagemFornecedor.java
+++ src/main/java/com/cadCompany/desafio/fornecedor/DadosListagemFornecedor.java
@@ -1,8 +1,8 @@
 package com.cadCompany.desafio.fornecedor;
 
-public record DadosListagemFornecedor(String nome, String cnpjOuCpf) {
+public record DadosListagemFornecedor(Long id, String nome, String cnpjOuCpf) {
     /*DTO Listagem Fornecedor*/
     public DadosListagemFornecedor(Fornecedor fornecedor){
-        this(fornecedor.getNome(), fornecedor.getCnpjOuCpf());
+        this(fornecedor.getId(), fornecedor.getNome(), fornecedor.getCnpjOuCpf());
     }
 }
