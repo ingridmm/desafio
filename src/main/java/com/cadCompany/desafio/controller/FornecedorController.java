2fb2e7074b3a30d7b86a5a6a397cbab3e12dd1a5
Ingrid M Miranda <ingridmoreiramiranda@gmail.com>
2023-06-24 22:15:11 -0300

    Adicionado: - Validação por cpf e cnpj;

--- src/main/java/com/cadCompany/desafio/controller/FornecedorController.java
+++ src/main/java/com/cadCompany/desafio/controller/FornecedorController.java
@@ -4,6 +4,8 @@ import com.cadCompany.desafio.fornecedor.*;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,10 +20,43 @@ public class FornecedorController {
 
     @PostMapping
     @Transactional //necessário ter uma anotação ativa no BD
-    public void cadastrar(@RequestBody @Valid DadosCadastraisFornecedor dados){
-        //leva a informação no corpo da requisição json, chama o repository pega os dados do DTO e converteu para entidade Forcenedor e salvou no BD
+    public ResponseEntity<String> cadastrar(@RequestBody @Valid DadosCadastraisFornecedor dados){
 
-        fornecedorRepository.save(new Fornecedor(dados));
+        String cnpjOuCpf = dados.cnpjOuCpf();
+        String cnpjOuCpfValidado = validarCnpjCpf(cnpjOuCpf);
+
+        if (cnpjOuCpfValidado.equals(cnpjOuCpf)) {
+
+            if (isCpf(cnpjOuCpf)) {
+
+                if (dados.dataNascimento() == null || dados.rg() == null) {
+                    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("É necessário informar a data de nascimento e RG para CPF.");
+                }
+                fornecedorRepository.save(new Fornecedor(dados));
+            } else {
+
+                fornecedorRepository.save(new Fornecedor(dados));
+            }
+        } else {
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("CNPJ ou CPF inválido");
+        }
+        return null;
+    }
+
+    public String validarCnpjCpf(String cnpjOuCpf){
+        if (cnpjOuCpf.length() == 11) { // CPF
+            var cpf = cnpjOuCpf.replaceAll("(\\d{3})(\\d{3})(\\d{3})(\\d{2})", "$1.$2.$3-$4");
+            return cpf;
+        } else if (cnpjOuCpf.length() == 14) { // CNPJ
+            var cnpj = cnpjOuCpf.replaceAll("(\\d{2})(\\d{3})(\\d{3})(\\d{4})(\\d{2})", "$1.$2.$3/$4-$5");
+            return cnpj;
+        }
+        return cnpjOuCpf;
+    }
+
+    private boolean isCpf(String cnpjOuCpf) {
+        return cnpjOuCpf.length() == 11;
     }
     @GetMapping
     public List<DadosListagemFornecedor> list(Pageable paginacao){
